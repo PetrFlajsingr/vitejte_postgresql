abdbea64deb8f349d06de5a42ab5fb236bf64da5
PetrFlajsingr <zizto1-Jatwyx-siqzac>
2021-02-11 13:52:54 +0100

    add tabulate and indicators

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "DeviceFinder.h"
+#include "fmt/core.h"
+#include "fmt/ostream.h"
 #include "global_logger.h"
+#include "indicators/indeterminate_progress_bar.hpp"
 #include "lib_interface/Communicator.h"
 #include "lib_interface/LibInterface.h"
+#include "range/v3/view/enumerate.hpp"
 #include "savers.h"
+#include "tabulate/table.hpp"
 #include "utils.h"
 #include <argparse.hpp>
 #include <chrono>
@@ -12,8 +17,9 @@
 #include <magic_enum.hpp>
 #include <pqxx/pqxx>
 #include <toml++/toml.h>
-#include "fmt/core.h"
-#include "fmt/ostream.h"
+#include <indicators/cursor_control.hpp>
+#include <indicators/termcolor.hpp>
+#include <future>
 
 using namespace std::string_literals;
 
@@ -55,18 +61,46 @@ argparse::ArgumentParser createArgumentParser() {
 
 void listDevices() {
   logger->log(spdlog::level::trace, "Vyhledavani Vitejte na siti");
-  const auto devices = vitejte::DeviceFinder{}.findDevices();
-  logger->log(spdlog::level::trace, "Vyhledavani dokonceno, pocet nalezenych zarizeni: {}", devices.size());
-  fmt::print("Nalezena zarizeni: \n");
-  auto i = 0u;
-  fmt::print("Index\tID\n");
-  for (const auto &device : devices) {
-    fmt::print("#{}\t{}\n", i, device);
-    ++i;
+
+  auto bar = indicators::IndeterminateProgressBar{
+      indicators::option::BarWidth{40},
+      indicators::option::Start{"["},
+      indicators::option::Fill{"-"},
+      indicators::option::Lead{"<==>"},
+      indicators::option::End{"]"},
+      indicators::option::PostfixText{"Vyhledavani zarizeni"},
+      indicators::option::ForegroundColor{indicators::Color::yellow},
+      indicators::option::FontStyles{std::vector<indicators::FontStyle>{indicators::FontStyle::bold}}};
+
+  auto job =[&] {
+    auto devices = vitejte::DeviceFinder{}.findDevices();
+    bar.mark_as_completed();
+    return devices;
+  };
+
+  auto devFuture = std::async(std::launch::async, job);
+
+  while (!bar.is_completed()) {
+    bar.tick();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
+
+  const auto devices = devFuture.get();
+
+
+  logger->log(spdlog::level::trace, "Vyhledavani dokonceno, pocet nalezenych zarizeni: {}", devices.size());
+
+  auto table = tabulate::Table();
+  table.format().font_style({tabulate::FontStyle::bold}).font_align(tabulate::FontAlign::left);
+  fmt::print("Nalezena zarizeni: \n");
+  table.add_row({"Index", "ID"});
+  for (const auto &[idx, device] : ranges::views::enumerate(devices)) {
+    table.add_row({fmt::format("#{}", idx + 1), device});
+  }
+  fmt::print("{}\n", table);
   fmt::print("Pro vyber zarizeni jej pridejte do konfiguracniho souboru 'config.toml' ve tvaru:\n");
   fmt::print("[vitejte]\n"
-               "id = \"id\"\n");
+             "id = \"id\"\n");
 }
 
 AppMode decideAppMode(argparse::ArgumentParser &args) {
@@ -80,9 +114,7 @@ int run(bool test, vitejte::SaverType saverType, const std::filesystem::path &ex
   auto config = toml::parse_file((exeFolder / "config.toml").string());
   auto dataSaver = vitejte::createDataSaver(saverType, config);
 
-  if (test) {
-    return vitejte::runSavingTest(*dataSaver);
-  }
+  if (test) { return vitejte::runSavingTest(*dataSaver); }
 }
 
 int main(int argc, char *argv[]) {
